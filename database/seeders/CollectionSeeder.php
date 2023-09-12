17fb1b311941e5e8e93d280ae6405dcb7f7a55de
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2023-09-12 10:19:37 -0300

    chore: create collection seeder

--- database/seeders/CollectionSeeder.php
+++ database/seeders/CollectionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Collection;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class CollectionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Collection::factory()->count(10)->create();
     }
 }
