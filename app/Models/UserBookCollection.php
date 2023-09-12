838a5af9171b267f0db01170490dbf76bff15eee
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2023-09-12 09:49:54 -0300

    chore: create relationship model

---
+++ app/Models/UserBookCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserBookCollection extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_book_collection';
+    protected $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'collection_id',
+    ];
+}
