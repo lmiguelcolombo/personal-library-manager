085f7772d747440cc418b9999fbecf0b421e9df3
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2023-09-12 10:39:02 -0300

    fix: removed model from the factories

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -10,13 +10,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class BookFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Book::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
