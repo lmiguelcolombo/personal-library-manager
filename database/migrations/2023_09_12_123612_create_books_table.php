7160226bca3c7ea0557b330b517ec90b0a61c6b2
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2023-09-12 09:42:52 -0300

    refactor: column title for the books table

--- database/migrations/2023_09_12_123612_create_books_table.php
+++ database/migrations/2023_09_12_123612_create_books_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->string('subject');
             $table->string('authors');
             $table->integer('edition');
