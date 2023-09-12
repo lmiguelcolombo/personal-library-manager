e5570f9f42730a8108755bca6e0e81d6f05565b3
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2023-09-12 09:43:13 -0300

    chore: add columns to the collections table

--- database/migrations/2023_09_12_123940_create_collections_table.php
+++ database/migrations/2023_09_12_123940_create_collections_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('collections', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
