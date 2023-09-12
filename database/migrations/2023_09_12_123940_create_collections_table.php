3eda9a0a20ad022b3c34e4d1ca354c2ab40543d6
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2023-09-12 09:39:58 -0300

    feat: create Collection structure

--- database/migrations/2023_09_12_123940_create_collections_table.php
+++ database/migrations/2023_09_12_123940_create_collections_table.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('collections', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('subject');
-            $table->string('authors');
-            $table->integer('edition');
-            $table->integer('publish_year');
-            $table->string('publisher');
             $table->timestamps();
         });
     }
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('collections');
     }
 };
